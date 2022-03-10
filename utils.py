4ff552fa358d8cae38b49c0167fa6edf99a0c269
Wonjun Ko <wjko@korea.ac.kr>
2022-03-10 16:41:45 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ from mne.filter import filter_data, notch_filter
 class load_dataset():
     def __init__(self, sbj_idx):
         self.sbj_idx = sbj_idx
-        self.path = '/home/ko/Desktop/pycharm-2018.3.5/projects/Data/BCIC_V_3/' # Define the data path
+        self.path = '/Define/Your/Own/Path' # Define the data path
 
     def load_data(self):
         data_tr = scipy.io.loadmat(self.path + f'Trainingset/Data_Sample{self.sbj_idx:02d}.mat')
